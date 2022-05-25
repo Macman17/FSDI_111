d6faa59712c1b144ac07e108c8aae6a4c943b40d
Naqui <Naqui17@gmail.com>
2022-05-25 07:43:34 -0700

    Create files assignment 1

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+import re
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    me = {
+        "first_name": "Naqui",
+        "last_name": "Darby",
+        "hobbies": "Basketball",
+    }
+    return me
